ca8648e71103d2de3a355bdc944e877646c6db1c
tao-isaman <towlovelove@gmail.com>
2021-11-15 14:41:12 +0700

    init eden logo

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,30 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
-import ProTip from './ProTip';
+import { Avatar, Grid } from '@mui/material';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
-}
 
 export default function App() {
   return (
     <Container maxWidth="sm">
+      <Grid container sx={{ mx: "auto" }} >
+      <Box
+        component="img"
+        sx={{
+          height: 100,
+          width: 100,
+          maxHeight: { xs: 100, md: 100 },
+          maxWidth: { xs: 100, md: 100 },
+        }}
+        alt="The house from the offer."
+        src="https://1.bp.blogspot.com/-gImT3poUoiw/YZINXuowy2I/AAAAAAAAFx0/EHJ56a_maE4G9ZKbDBMafWauhhmmCuPjACLcBGAsYHQ/s320/eden_logo-removebg-preview.png"
+      />
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           Eden Project
         </Typography>
-        <ProTip />
-        <Copyright />
       </Box>
+      </Grid>
     </Container>
   );
 }
